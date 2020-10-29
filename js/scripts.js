a38af9c67a642f28fc82553fdfc72ca0a83dab2d
patrick <mangandhy2001@gmail.com>
2020-10-29 15:27:00 -0500

    v0.05

--- js/scripts.js
+++ js/scripts.js
@@ -4,71 +4,73 @@
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
     */
     (function ($) {
-    "use strict"; // Start of use strict
+      ("use strict"); // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+      // Smooth scrolling using jQuery easing
+      $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
+          location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+          location.hostname == this.hostname
         ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top - 70,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
+          var target = $(this.hash);
+          target = target.length
+            ? target
+            : $("[name=" + this.hash.slice(1) + "]");
+          if (target.length) {
+            $("html, body").animate(
+              {
+                scrollTop: target.offset().top - 70,
+              },
+              1000,
+              "easeInOutExpo"
+            );
+            return false;
+          }
         }
-    });
+      });
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
+      // Closes responsive menu when a scroll trigger link is clicked
+      $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
-    });
+      });
 
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
+      // Activate scrollspy to add active class to navbar items on scroll
+      $("body").scrollspy({
         target: "#mainNav",
         offset: 100,
-    });
+      });
 
-    // Collapse Navbar
-    var navbarCollapse = function () {
+      // Collapse Navbar
+      var navbarCollapse = function () {
         if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
+          $("#mainNav").addClass("navbar-shrink");
         } else {
-            $("#mainNav").removeClass("navbar-shrink");
+          $("#mainNav").removeClass("navbar-shrink");
         }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-})(jQuery); // End of use strict
+      };
+      // Collapse now if page is not at top
+      navbarCollapse();
+      // Collapse the navbar when page is scrolled
+      $(window).scroll(navbarCollapse);
 
-// Magnific popup calls
- $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
+      // Magnific popup calls
+      $("#portfolio").magnificPopup({
+        delegate: "a",
+        type: "image",
+        tLoading: "Loading image #%curr%...",
+        mainClass: "mfp-img-mobile",
+        gallery: {
+          enabled: true,
+          navigateByImgClick: true,
+          preload: [0, 1],
+        },
+        image: {
+          tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        },
+      });
+    })(jQuery); // End of use strict
+
+
 
   
