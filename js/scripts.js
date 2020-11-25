0f0644ed5020770ac18517a7b4a4925a682d175e
patrick <mangandhy2001@gmail.com>
2020-11-25 12:55:49 -0500

    v0.0.9

--- js/scripts.js
+++ js/scripts.js
@@ -1,117 +1,109 @@
 /*!
-    * Start Bootstrap - Grayscale v6.0.2 (https://startbootstrap.com/themes/grayscale)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-    */
+ * Start Bootstrap - Grayscale v6.0.2 (https://startbootstrap.com/themes/grayscale)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
+ */
 
-    window.sr = ScrollReveal();
+window.sr = ScrollReveal();
 
-    sr.reveal('.header-content-top', {
-      duration: 2500,
-      origin: 'top',
-      distance: '300px'
-    });
+sr.reveal(".header-content-top", {
+  duration: 2500,
+  origin: "top",
+  distance: "300px",
+});
 
-    sr.reveal('.header-content-right', {
-      duration: 2500,
-      origin: 'right',
-      distance: '100px'
-    });
+sr.reveal(".header-content-right", {
+  duration: 2500,
+  origin: "right",
+  distance: "100px",
+});
 
-    sr.reveal('.header-content-left-1', {
-      duration: 2500,
-      origin: 'left',
-      distance: '100px'
-    });
+sr.reveal(".header-content-left-1", {
+  duration: 2500,
+  origin: "left",
+  distance: "100px",
+});
 
-    sr.reveal('.header-content-left-2', {
-      duration: 3000,
-      origin: 'left',
-      distance: '100px'
-    });
+sr.reveal(".header-content-left-2", {
+  duration: 3000,
+  origin: "left",
+  distance: "100px",
+});
 
-    sr.reveal('.header-content-left-3', {
-      duration: 3500,
-      origin: 'left',
-      distance: '100px'
-    });
+sr.reveal(".header-content-left-3", {
+  duration: 3500,
+  origin: "left",
+  distance: "100px",
+});
 
-    sr.reveal('.header-content-left-4', {
-      duration: 4000,
-      origin: 'left',
-      distance: '100px'
-    });
+sr.reveal(".header-content-left-4", {
+  duration: 4000,
+  origin: "left",
+  distance: "100px",
+});
 
-    (function ($) {
-      ("use strict"); // Start of use strict
+(function ($) {
+  ("use strict"); // Start of use strict
 
-      // Smooth scrolling using jQuery easing
-      $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-          location.pathname.replace(/^\//, "") ==
-            this.pathname.replace(/^\//, "") &&
-          location.hostname == this.hostname
-        ) {
-          var target = $(this.hash);
-          target = target.length
-            ? target
-            : $("[name=" + this.hash.slice(1) + "]");
-          if (target.length) {
-            $("html, body").animate(
-              {
-                scrollTop: target.offset().top - 70,
-              },
-              1000,
-              "easeInOutExpo"
-            );
-            return false;
-          }
-        }
-      });
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 70,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
 
-      // Closes responsive menu when a scroll trigger link is clicked
-      $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-      });
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-      // Activate scrollspy to add active class to navbar items on scroll
-      $("body").scrollspy({
-        target: "#mainNav",
-        offset: 100,
-      });
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 100,
+  });
 
-      // Collapse Navbar
-      var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-          $("#mainNav").addClass("navbar-shrink");
-        } else {
-          $("#mainNav").removeClass("navbar-shrink");
-        }
-      };
-      // Collapse now if page is not at top
-      navbarCollapse();
-      // Collapse the navbar when page is scrolled
-      $(window).scroll(navbarCollapse);
+  // Collapse Navbar
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
 
-      // Magnific popup calls
-      $("#portfolio").magnificPopup({
-        delegate: "a",
-        type: "image",
-        tLoading: "Loading image #%curr%...",
-        mainClass: "mfp-img-mobile",
-        gallery: {
-          enabled: true,
-          navigateByImgClick: true,
-          preload: [0, 1],
-        },
-        image: {
-          tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-        },
-      });
-    })(jQuery); // End of use strict
-
-
-
-
-
-  
+  // Magnific popup calls
+  $("#portfolio").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1],
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    },
+  });
+})(jQuery); // End of use strict
